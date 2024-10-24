09882dc408588b3cf77b2e025a5c81082685890a
thng292 <nguyenquangthong292@gmail.com>
2024-10-24 09:47:04 +0700

    chore: Added script to filter and upload leetcode problems to huggingface

--- src/main.py
+++ src/main.py
@@ -9,14 +9,6 @@ load_dotenv()
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
-    model = genai.GenerativeModel("gemini-1.5-flash")
-    response = model.generate_content("Write a story about a magic backpack.")
-    return response.to_dict()
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return {"Hello": "World!"}
