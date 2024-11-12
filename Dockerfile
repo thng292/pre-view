2de4c64df429e468d61e4d8c048ca0020dee3c0a
cutehusky <liyihong102@gmail.com>
2024-11-12 10:16:57 +0700

    add: docker to build frontend

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,24 @@
 # Read the doc: https://huggingface.co/docs/hub/spaces-sdks-docker
 # you will also find guides on how best to write your Dockerfile
 
-FROM python:3.12
+FROM nikolaik/python-nodejs:python3.10-nodejs18
 
+#FROM python:3.12
+
+WORKDIR /app
+
+COPY . /app 
+#RUN apt-get update
+#RUN apt-get -y install nodejs
+#RUN apt-get -y install npm
+
+WORKDIR /app/pre-view-frontend
+RUN npm install
+RUN npm run build
 WORKDIR /app
 
 RUN python -m pip install --upgrade pip
 COPY ./requirements.txt requirements.txt
 RUN pip install --no-cache-dir --upgrade -r requirements.txt
 
-COPY . /app
 CMD ["fastapi", "run", "src/main.py"]
--- src/main.py
+++ src/main.py
@@ -3,12 +3,23 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 import google.generativeai as genai
 import os
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
 
+'''
 load_dotenv('.env')
-
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+'''
+
 app = FastAPI()
 
+templates = Jinja2Templates(directory="pre-view-frontend/dist")
+
+app.mount('/assets', StaticFiles(directory="pre-view-frontend/dist/assets"), 'static')
+#app.mount('/unity', StaticFiles(directory="pre-view-frontend/unity"), 'static')
+
 @app.get("/")
-def read_root():
-    return {"Hello": "World!"}
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
