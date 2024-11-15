4068510f4d385df481cde504f92ecacd1bbc2d99
thng292 <nguyenquangthong292@gmail.com>
2024-11-15 07:25:55 +0700

    fix:Update extract job description

---
+++ src/extract_jd.py
@@ -0,0 +1,92 @@
+from enum import StrEnum
+from pydantic import BaseModel
+from google.generativeai import GenerativeModel, GenerationConfig
+import json
+
+SYSTEM_INSTRUCTION = """
+You are a senior developer. You will assist a friend finding job."""
+
+model = GenerativeModel("gemini-1.5-flash-002", system_instruction=SYSTEM_INSTRUCTION)
+
+
+class JobLevel(StrEnum):
+    intern = "intern"
+    fresher = "fresher"
+    junior = "junior"
+    mid_level = "mid-level"
+    senior = "senior"
+
+
+class ProblemDifficulty(StrEnum):
+    easy = "easy"
+    medium = "medium"
+    hard = "hard"
+
+
+class ProblemTag(StrEnum):
+    hashTable = "Hash Table"
+    math = "Math"
+    twoPointers = "Two Pointers"
+    depthFirstSearch = "Depth-First Search"
+    stack = "Stack"
+    numberTheory = "Number Theory"
+    gameTheory = "Game Theory"
+    linkedList = "Linked List"
+    tree = "Tree"
+    dynamicProgramming = "Dynamic Programming"
+    memoization = "Memoization"
+    reservoirSampling = "Reservoir Sampling"
+    trie = "Trie"
+    set = "Set"
+    matrix = "Matrix"
+    greedy = "Greedy"
+    recursion = "Recursion"
+    graph = "Graph"
+    bitmask = "Bitmask"
+    unionFind = "Union Find"
+    queue = "Queue"
+    breadthFirstSearch = "Breadth-First Search"
+    divideAndConquer = "Divide and Conquer"
+    sorting = "Sorting"
+    string = "String"
+    backtracking = "Backtracking"
+    binarySearch = "Binary Search"
+    array = "Array"
+    prefixSum = "Prefix Sum"
+    geometry = "Geometry"
+    shortestPath = "Shortest Path"
+    priorityQueue = "Priority Queue"
+    slidingWindow = "Sliding Window"
+
+
+class JobExtractedOutput(BaseModel):
+    companyName: str | None
+    jobTitle: str
+    jobLevel: JobLevel
+    yoe: int
+    programmingLanguages: list[str]
+    frameworks: list[str]
+    skills: list[str]
+    responsibilities: list[str]
+    problems: list[ProblemTag]
+    problemDifficulty: ProblemDifficulty
+    interviewTopics: list[str]
+
+
+def extractJD(jd: str, retry: int = 1):
+    for i in range(retry):
+        response = model.generate_content(
+            jd.strip(),
+            generation_config=GenerationConfig(
+                temperature=i * 0.2,
+                response_mime_type="application/json",
+                response_schema=JobExtractedOutput,
+            ),
+        )
+        try:
+            print(response.text)
+            return JobExtractedOutput(**json.loads(response.text))
+        except Exception as e:
+            print(e)
+            pass
+    return None
--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import os, dotenv, logging, json, time
 from fastapi import FastAPI, HTTPException
-from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from typing import Any
 import google.generativeai as genai
+
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -17,73 +17,18 @@ gemini_flash = genai.GenerativeModel(model_name="gemini-1.5-flash")
 app = FastAPI()
 
 
-# Define a Pydantic model to structure the expected response
 class JobExtractInput(BaseModel):
     jd: str
 
 
-class JobExtractedOutput(BaseModel):
-    companyName: Any
-    jobTitle: Any
-    jobLevel: Any
-    yoe: Any
-    programmingLanguages: list[Any]
-    frameworks: list[Any]
-    skills: list[Any]
-    responsibilities: list[Any]
+from extract_jd import JobExtractedOutput, extractJD
 
 
-# Define the endpoint for submitting the job description
 @app.post("/extract-job-info/", response_model=JobExtractedOutput)
 async def extract_job_info(request: JobExtractInput):
     job_description = request.jd
 
-    # Define the prompt template
-    prompt = f"""This is a job description of a job related to IT field: 
-    \n\n{job_description}\n\nExtract the following information from the above paragraph and export it to valid JSON:
-    Company name
-    Job title
-    Job level (intern, fresher, junior, senior)
-    Year of experience (yoe)
-    Programming languages
-    Frameworks
-    Skills
-    Responsibilities
-
-    Example:
-    {{
-        "companyName": "Google",
-        "jobTitle": "Software developer",
-        "jobLevel": "junior",
-        "yoe": 1,
-        "programmingLanguages": ["HTML", "CSS", "JavaScript"],
-        "frameworks": ["ReactJS"],
-        "skills": [
-            "Object Oriented Programming",
-            "Design Patterns",
-            "Windows/Linux/MacOS development"
-        ],
-        "responsibilities": [
-            "Analyze, design, develop and test software product features",
-            "Produce technical solutions and implement them"
-        ]
-    }}
-
-    Do not use markdown.
-    Do not add more words to the JSON output; if you want to add a little thought or notes, use a comment instead."""
-
-    # Send the prompt to the Google Gemini API
-    retry = 3
-    for i in range(retry):
-        try:
-            logger.info("Sending request to gemini")
-            before = time.time()
-            response = gemini_flash.generate_content(
-                prompt, generation_config={"temperature": i * 0.1}
-            )
-            logger.info("Gemini time taken %f", time.time() - before)
-            return JobExtractedOutput(**json.loads(response.text))
-        except Exception as e:
-            logger.error("Failed to parse JSON: %s", str(e))
-
+    output = extractJD(job_description)
+    if output:
+        return output
     raise HTTPException(status_code=400, detail=f"Couldn't process this")
